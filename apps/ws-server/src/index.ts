93aa1809a7aec7272c996598f2c347a12b54d02a
HarshJ <harshjain3575@gmail.com>
2025-02-04 09:41:41 +0530

    feat(ws-server): added state mngt in ws server

--- apps/ws-server/src/index.ts
+++ apps/ws-server/src/index.ts
@@ -1,7 +1,31 @@
-import { WebSocketServer } from "ws";
+import { WebSocketServer, WebSocket } from "ws";
 import jwt, { JwtPayload } from "jsonwebtoken"; 
+import { JWT_SECRET } from "@repo/backend-common/config"
 
 const wss = new WebSocketServer({ port: 3002 });
+
+type User = {
+  rooms: string[],
+  ws: WebSocket 
+}
+
+type Data = {
+  type: 'join-room' | 'create-room' | 'leave-room' | 'chat',
+  roomId: string,
+  chatMessage?: string
+}
+
+const users: User[] = []
+
+const checkValid = (token: string) : null | string => {
+  const decoded = jwt.verify(token, JWT_SECRET) as JwtPayload;
+  
+  if(!decoded || !decoded.userdb.id) {
+    return null;
+  }
+
+  return decoded.userdb.id;
+}
 
 wss.on('connection', async (ws, request) => {
   const url = request.url;
@@ -9,12 +33,45 @@ wss.on('connection', async (ws, request) => {
 
   const queryParams = new URLSearchParams(url.split('?')[1]);
   const token = queryParams.get('token') || "";
-  const decoded = jwt.verify(token, "asdas") as JwtPayload;
-  
-  if(!decoded || !decoded.userdb.id) {
+  const user = checkValid(token);
+  if(!user) {
     ws.close();
     return;
   }
 
-  ws.send("valid user");
+  users.push({
+    ws,
+    rooms: []
+  });
+
+
+  ws.on('message', (message: string) => {
+    try {
+      const data: Data = JSON.parse(message);
+
+      if(data.type === 'join-room') {
+        const user = users.find(user => user.ws === ws);
+        user?.rooms.push(data.roomId);
+      }
+      if(data.type === 'leave-room') {
+        const user = users.find(user => user.ws === ws);
+        if(!user) return;
+        user.rooms = user?.rooms.filter(room => room === data.roomId)
+      }
+      if(data.type === 'chat') {
+        users.forEach(user => {
+          if(user.rooms.includes(data.roomId)) {
+            user.ws.send(JSON.stringify({
+              type: 'chat',
+              message: data.chatMessage,
+              roomId: data.roomId
+            }));
+          }
+        });
+      }
+
+    } catch (error) {
+      return null;
+    }
+  });
 })
